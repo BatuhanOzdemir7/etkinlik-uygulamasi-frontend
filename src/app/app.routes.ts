93cde99e7577410ff6751d9a170d21a58f915066
Batuhan Özdemir <batuhan77demir@gmail.com>
2026-05-17 12:54:35 +0300

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Login}, //sayfaya ilk defa giriş yaptığında login sayfasını göstermek için
+    {path: 'register', component: Register} //kayıt sayfasına yönlendirmek için
+];
